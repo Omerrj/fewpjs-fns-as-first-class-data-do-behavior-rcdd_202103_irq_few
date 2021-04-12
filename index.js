43059677c4027c20f9cbd96b3ebb445c5f0adb37
Omer Sabah <omerrj99@gmail.com>
2021-04-12 12:32:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function greet(timeString){
     return 'Good Morning'
   }else if(hour>=12&&hour<=17){
     return 'Good Afternoon'
-  }else
+  }else return 'Good Evening'
 
 }
 /* Write your implementation of displayMessage() */
