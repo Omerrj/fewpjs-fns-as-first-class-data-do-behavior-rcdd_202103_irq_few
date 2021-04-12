714c2e52e37cf91169860ea42f62975d1b4e5ca3
Omer Sabah <omerrj99@gmail.com>
2021-04-12 12:28:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 function greet(timeString){
-  const hour=timeString.split(0,1);
-  const min=timeString.split(3,5);
+  const hour=timeString.splice(0,1);
+  const min=timeString.splice(3,5);
   console.log(hour,min)
 }
 
