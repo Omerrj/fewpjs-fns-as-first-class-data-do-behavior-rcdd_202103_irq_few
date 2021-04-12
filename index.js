9647709752c27afc451791a0edc24810de109aa5
Omer Sabah <omerrj99@gmail.com>
2021-04-12 12:29:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ function handleClick(e) {
 function greet(timeString){
   const hour=timeString.slice(0,2);
   const min=timeString.slice(3,5);
-  console.log(hour,min)
-}
 
-greet('12:20');
+}
 /* Write your implementation of displayMessage() */
